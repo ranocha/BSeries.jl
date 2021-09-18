e7765d31f59e6722d67cf52ecc1d2007f372e80d
Hendrik Ranocha <mail@ranocha.de>
2021-09-18 08:10:12 +0200

    test substitute

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,31 @@
 using Test
 using BSeries
 
+using Symbolics: Symbolics, @variables, Num
+
+
+@testset "subsitution" begin
+  @variables a1 a2 a31 a32
+  a = OrderedDict{RootedTrees.RootedTree{Int, Vector{Int}}, Num}(
+        rootedtree(Int[])     => 1,
+        rootedtree([1])       => a1,
+        rootedtree([1, 2])    => a2,
+        rootedtree([1, 2, 2]) => a31,
+        rootedtree([1, 2, 3]) => a32)
+
+  @variables b1 b2 b31 b32
+  b = OrderedDict{RootedTrees.RootedTree{Int, Vector{Int}}, Num}(
+        rootedtree(Int[])     => 0,
+        rootedtree([1])       => b1,
+        rootedtree([1, 2])    => b2,
+        rootedtree([1, 2, 2]) => b31,
+        rootedtree([1, 2, 3]) => b32)
+
+  t = rootedtree([1])
+  @inferred substitute(b, a, t)
+  @test isequal(substitute(b, a, t), a1 * b1)
+
+  t = rootedtree([1, 2, 3])
+  @inferred substitute(b, a, t)
+  @test isequal(substitute(b, a, t), a1 * b32 + a32 * b1^3 + 2 * a2 * b1 * b2)
+end
