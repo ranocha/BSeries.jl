a9395419ca71a509a5683e7dbbc2ab65df6827bf
Hendrik Ranocha <mail@ranocha.de>
2021-09-18 12:38:37 +0200

    construct B-series of the modified equation

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -8,7 +8,9 @@ using RootedTrees: RootedTree
 @reexport using OrderedCollections: OrderedDict
 
 
-export substitute
+export bseries, substitute
+
+export modified_equation
 
 
 """
@@ -26,6 +28,97 @@ function substitute(b, a, t::RootedTree)
   end
 
   return result
+end
+
+
+
+"""
+    bseries(A::AbstractMatrix, b::AbstractVector, c::AbstractVector, order)
+
+Compute the B-series of the  Runge-Kutta method with Butcher coefficients
+`A, b, c` up to a prescribed `order`
+"""
+function bseries(A::AbstractMatrix, b::AbstractVector, c::AbstractVector, order)
+  T = promote_type(eltype(A), eltype(b), eltype(c))
+  series = OrderedDict{RootedTree{Int, Vector{Int}}, T}()
+
+  for o in 1:order
+    for t in RootedTreeIterator(o)
+      series[copy(t)] = elementary_weight(t, A, b, c)
+    end
+  end
+
+  return series
+end
+
+
+"""
+    ExactSolution{T}
+
+Lazy representation of the B-series of the exact solution of an ordinary
+differential equation using coefficients of type at least as representative as
+`T`.
+"""
+struct ExactSolution{T} end
+
+Base.getindex(::ExactSolution{T}, t::RootedTree) where {T} = one(T) / γ(t)
+
+Base.IteratorSize(::Type{<:ExactSolution}) = Base.SizeUnknown()
+Base.eltype(::Type{ExactSolution{T}}) where {T} = promote_type(T, Int)
+
+function Base.iterate(exact::ExactSolution)
+  iterator = RootedTreeIterator(1)
+  t, state = iterate(iterator)
+  (exact[t], (state, iterator))
+end
+
+function Base.iterate(exact::ExactSolution, state_iterator)
+  state, iterator = state_iterator
+  t_state = iterate(iterator, state)
+  if t_state === nothing
+    iterator = RootedTreeIterator(iterator.order + 1)
+    t_state = iterate(iterator)
+  end
+  t, state = t_state
+  (exact[t], (state, iterator))
+end
+
+
+
+"""
+    modified_equation(A, b, c, order)
+
+Compute the B-series of the modified equation of the Runge-Kutta method with
+Butcher coefficients `A, b, c` up to a prescribed `order`.
+"""
+function modified_equation(A, b, c, order)
+  # B-series of the Runge-Kutta method
+  series_rk = bseries(A, b, c, order)
+
+  # B-series of the exact solution
+  T = eltype(values(series_rk))
+  series_ex = ExactSolution{T}()
+
+  # Prepare B-series of the modified equation
+  series = empty(series_rk)
+  for t in keys(series_rk)
+    series[t] = zero(T)
+  end
+
+  t = rootedtree([1])
+  series[t] = series_rk[t]
+
+  # Recursively solve `substitute(series, series_ex, t) == series_rk[t]`.
+  # This works because
+  #   substitute(series, series_ex, t) = series[t] + lower order terms
+  for o in 2:order
+    for _t in RootedTreeIterator(o)
+      t = copy(_t)
+      series[t] += series_rk[t] - substitute(series, series_ex, t)
+    end
+  end
+
+  return series
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using BSeries
 
+using StaticArrays: @SArray
 using Symbolics: Symbolics, @variables, Num
 
 
@@ -27,5 +28,54 @@ using Symbolics: Symbolics, @variables, Num
 
   t = rootedtree([1, 2, 3])
   @inferred substitute(b, a, t)
-  @test isequal(substitute(b, a, t), a1 * b32 + a32 * b1^3 + 2 * a2 * b1 * b2)
+  @test isequal(substitute(b, a, t), a1 * b32 + 2 * a2 * b1 * b2 + a32 * b1^3)
+end
+
+
+@testset "modified equation" begin
+  t1  = rootedtree([1])
+  t2  = rootedtree([1, 2])
+  t31 = rootedtree([1, 2, 2])
+  t32 = rootedtree([1, 2, 3])
+  t41 = rootedtree([1, 2, 2, 2])
+  t42 = rootedtree([1, 2, 2, 3])
+  t43 = rootedtree([1, 2, 3, 3])
+  t44 = rootedtree([1, 2, 3, 4])
+  t51 = rootedtree([1, 2, 2, 2, 2])
+  t52 = rootedtree([1, 2, 2, 2, 3])
+  t53 = rootedtree([1, 2, 2, 3, 3])
+  t54 = rootedtree([1, 2, 2, 3, 4])
+  t55 = rootedtree([1, 2, 3, 2, 3])
+  t56 = rootedtree([1, 2, 3, 3, 3])
+  t57 = rootedtree([1, 2, 3, 3, 4])
+  t58 = rootedtree([1, 2, 3, 4, 4])
+  t59 = rootedtree([1, 2, 3, 4, 5])
+
+  # SSPRK(2, 2) aka Heun's method
+  A = @SArray [0.0 0.0; 1.0 0.0]
+  b = @SArray [1/2, 1/2]
+  c = @SArray [0.0, 1.0]
+  order = 5
+  series = modified_equation(A, b, c, order)
+
+  # tested with the python package BSeries
+  @test series[t1 ] ≈ 1.0                   atol=10*eps()
+  @test series[t2 ] ≈ 0                     atol=10*eps()
+  @test series[t31] ≈ 0.166666666666667     atol=10*eps()
+  @test series[t32] ≈ -0.166666666666667    atol=10*eps()
+  @test series[t41] ≈ -1.11022302462516e-16 atol=10*eps()
+  @test series[t42] ≈ -0.125000000000000    atol=10*eps()
+  @test series[t43] ≈ -2.77555756156289e-17 atol=10*eps()
+  @test series[t44] ≈ 0.125000000000000     atol=10*eps()
+  @test series[t51] ≈ -0.0333333333333332   atol=10*eps()
+  @test series[t52] ≈ -0.0583333333333333   atol=10*eps()
+  @test series[t55] ≈ 0.0750000000000000    atol=10*eps()
+  @test series[t53] ≈ 0.0583333333333333    atol=10*eps()
+  @test series[t56] ≈ 0.0333333333333334    atol=10*eps()
+  @test series[t54] ≈ 0.0500000000000000    atol=10*eps()
+  @test series[t57] ≈ 0.0583333333333333    atol=10*eps()
+  @test series[t58] ≈ -0.0583333333333333   atol=10*eps()
+  @test series[t59] ≈ -0.0500000000000000   atol=10*eps()
+
+
 end
