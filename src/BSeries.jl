29c26496c85a6c40538af342dfce11db2a075a0a
Hendrik Ranocha <mail@ranocha.de>
2021-09-18 08:10:03 +0200

    src/BSeries.jl
    Co-authored-by: David Ketcheson <dketch@gmail.com>

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -3,7 +3,30 @@ module BSeries
 using Reexport: @reexport
 
 @reexport using RootedTrees
+using RootedTrees: RootedTree
 
+@reexport using OrderedCollections: OrderedDict
+
+
+export substitute
+
+
+"""
+    substitute(b, a, t::RootedTree)
+
+Compute the coefficient correspoding to the tree `t` of the B-series that is
+formed by substituting the B-series `b` into the B-series `a`.
+"""
+function substitute(b, a, t::RootedTree)
+  forests, skeletons = all_partitions(t)
+  result = zero(first(values(a)) * first(values(b)))
+
+  for (forest, skeleton) in zip(forests, skeletons)
+    result += reduce(*, b[tree] for tree in forest) * a[skeleton]
+  end
+
+  return result
+end
 
 
 end # module
