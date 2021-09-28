3cb0b2cc101ee7860aa9dd8da0ab4f9c89d7bf5e
Hendrik Ranocha <mail@ranocha.de>
2021-09-28 19:38:03 +0200

    test SymEngine.jl, SymPy.jl, and Symbolics.jl

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -143,7 +143,7 @@ end
 
 
 """
-    ExactSolution{T}
+    ExactSolution{T}()
 
 Lazy representation of the B-series of the exact solution of an ordinary
 differential equation using coefficients of type at least as representative as
@@ -240,8 +240,15 @@ Butcher coefficients `A, b, c` up to the prescribed `order` with respect to
 the ordinary differential equation ``u'(t) = f(u(t))`` with vector field `f`
 and dependent variables `u` for a time step size `dt`.
 
-Here, `u` is assumed to be a vector of variables from Symbolics.jl (`Symbolics.Num`)
-and `f` is assumed to be a vector of expressions in these variables.
+Here, `u` is assumed to be a vector of symbolic variables and `f` is assumed
+to be a vector of expressions in these variables. Currently, symbolic variables
+from
+
+- [SymEngine.jl](https://github.com/symengine/SymEngine.jl),
+- [SymPy.jl](https://github.com/JuliaPy/SymPy.jl), and
+- [Symbolics.jl](https://github.com/JuliaSymbolics/Symbolics.jl)
+
+are supported.
 
 # References
 
@@ -330,8 +337,15 @@ method with Butcher coefficients `A, b, c` up to the prescribed `order` with
 respect to the ordinary differential equation ``u'(t) = f(u(t))`` with vector
 field `f` and dependent variables `u` for a time step size `dt`.
 
-Here, `u` is assumed to be a vector of variables from Symbolics.jl (`Symbolics.Num`)
-and `f` is assumed to be a vector of expressions in these variables.
+Here, `u` is assumed to be a vector of symbolic variables and `f` is assumed
+to be a vector of expressions in these variables. Currently, symbolic variables
+from
+
+- [SymEngine.jl](https://github.com/symengine/SymEngine.jl),
+- [SymPy.jl](https://github.com/JuliaPy/SymPy.jl), and
+- [Symbolics.jl](https://github.com/JuliaSymbolics/Symbolics.jl)
+
+are supported.
 
 # References
 
