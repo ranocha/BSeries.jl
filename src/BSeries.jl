e88c66a28bc6835662a7b2724a5e6b326bb87515
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-10-07 21:16:22 +0200

    improve latexify (#30)
    * improve latexify
    
    * further improvements of latexify

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -725,18 +725,43 @@ function __init__()
 
       expressions = []
       for (t, val) in series
+        iszero(val) && continue
         elementary_differential = LatexifyElementaryDifferential(t, f)
         if dt isa Symbol || dt isa AbstractString
           # insert the symbol of dt
-          push!(expressions,
-                :($(val / symmetry(t)) *
-                  $dt^$(order(t) - reduce_order_by) *
-                  $(elementary_differential)))
+          val_symmetry = val / symmetry(t)
+          reduced_order = order(t) - reduce_order_by
+          if isone(val_symmetry)
+            if iszero(reduced_order)
+              push!(expressions,
+                    :($(elementary_differential)))
+            elseif isone(reduced_order)
+              push!(expressions,
+                    :($dt *
+                      $(elementary_differential)))
+            else
+              push!(expressions,
+                    :($dt^$(reduced_order) *
+                      $(elementary_differential)))
+            end
+          else
+            push!(expressions,
+                  :($(val / symmetry(t)) *
+                    $dt^$(order(t) - reduce_order_by) *
+                    $(elementary_differential)))
+          end
         else
           # assume that we can do arithmetic with dt
-          push!(expressions,
-                :($(val / symmetry(t) * dt^(order(t) - reduce_order_by)) *
-                  $(elementary_differential)))
+          factor = val / symmetry(t) * dt^(order(t) - reduce_order_by)
+          iszero(factor) && continue
+          if isone(factor)
+            push!(expressions,
+                  :($(elementary_differential)))
+          else
+            push!(expressions,
+                  :($(factor) *
+                    $(elementary_differential)))
+          end
         end
       end
       result = expressions[1]
