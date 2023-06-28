3691f695096a8b42ea08d3ab26ae88a5cbf4aab1
Sondar74 <79122393+Sondar74@users.noreply.github.com>
2023-06-28 10:40:19 +0300

    Update src/BSeries.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -678,10 +678,10 @@ TruncatedBSeries{RootedTree{Int64, Vector{Int64}}, Rational{Int64}} with 9 entri
 ```
 # References
 - Yuto Miyatake and John C. Butcher.
-"A characterization of energy-preserving methods and the construction of
-parallel integrators for Hamiltonian systems."
-SIAM Journal on Numerical Analysis 54, no. 3 (2016): 
-[DOI: 10.1137/15M1020861](https://doi.org/10.1137/15M1020861) 
+  "A characterization of energy-preserving methods and the construction of
+  parallel integrators for Hamiltonian systems."
+  SIAM Journal on Numerical Analysis 54, no. 3 (2016): 
+  [DOI: 10.1137/15M1020861](https://doi.org/10.1137/15M1020861) 
 """
 function bseries(csrk::ContinuousStageRungeKuttaMethod, order)
     csrk = csrk.matrix
