44f1d2955bf68be2b11f71a9e8b4595a740cae74
Hendrik Ranocha <mail@ranocha.de>
2023-06-28 11:28:39 +0200

    fix docstring of energy_preserving_order

--- src/BSeries.jl
+++ src/BSeries.jl
@@ -1547,12 +1547,12 @@ function renormalize!(series)
 end
 
 """
-    energy_preserving(rk::RungeKuttaMethod, max_order)
+    energy_preserving_order(rk::RungeKuttaMethod, max_order)
 
-This function checks up to which order a Runge-Kitta method 'rk' is
+This function checks up to which order a Runge-Kutta method `rk` is
 energy-preserving for Hamiltonian problems.
-It requires a 'max_order' so that it does not run forever if the order up to
-which the method is energy_preserving is too big or infinite.
+It requires a `max_order` so that it does not run forever if the order up to
+which the method is energy-preserving is too big or infinite.
 
 See also [`is_energy_preserving`](@ref)
 """
