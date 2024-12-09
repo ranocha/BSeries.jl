626bd8d610718c7cf67447801eda1633eeb5a9eb
Documenter.jl <documenter@juliadocs.github.io>
2024-12-09 16:42:25 +0000

    delete history

---
+++ v0.1.30/benchmark_python_pybs.py
@@ -0,0 +1,43 @@
+import os
+directory = os.path.dirname(os.path.abspath(__file__))
+io_file = os.path.join(directory, "benchmark_python_pybs.txt")
+
+import sys
+from importlib.metadata import version
+with open(io_file, 'w') as io:
+  print("Python version", sys.version, file=io)
+  print("Package version", version('pybs'), file=io)
+
+import time
+import pybs
+from pybs.rungekutta import methods as rk_methods
+
+midpoint_method = rk_methods.RKmidpoint
+up_to_order = 9
+number_of_terms = pybs.unordered_tree.number_of_trees_up_to_order(up_to_order+1)
+
+from itertools import islice
+def first_values(f, n):
+  return (f(tree) for tree in islice(pybs.unordered_tree.tree_generator(), 0, n))
+
+
+with open(io_file, 'a') as io:
+  print("\nModified equation", file=io)
+
+start_time = time.time()
+midpoint_series = midpoint_method.phi()
+series = pybs.series.modified_equation(midpoint_series)
+result = sum(first_values(series, number_of_terms))
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
+
+start_time = time.time()
+midpoint_series = midpoint_method.phi()
+series = pybs.series.modified_equation(midpoint_series)
+result = sum(first_values(series, number_of_terms))
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
