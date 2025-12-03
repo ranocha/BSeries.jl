6f2939445b13c9185bd6cf652c898cc96460b434
Documenter.jl <documenter@juliadocs.github.io>
2025-12-03 09:43:20 +0000

    delete history

---
+++ v0.1.64/benchmark_python_orderconditions.py
@@ -0,0 +1,66 @@
+import os
+directory = os.path.dirname(os.path.abspath(__file__))
+io_file = os.path.join(directory, "benchmark_python_orderconditions.txt")
+
+import sys
+with open(io_file, 'w') as io:
+  print("Python version", sys.version, file=io)
+
+import time
+from orderConditions import BSeries
+import nodepy.runge_kutta_method as rk
+
+midpoint_method = rk.loadRKM("Mid22")
+up_to_order = 9
+
+
+with open(io_file, 'a') as io:
+  print("\nModified equation", file=io)
+
+start_time = time.time()
+BSeries.set_order(up_to_order)
+Y1 = BSeries.y() + midpoint_method.A[1,0] * BSeries.hf()
+rk2 = BSeries.y() + midpoint_method.b[0] * BSeries.hf() + midpoint_method.b[1] * BSeries.compo_hf(Y1)
+series = BSeries.modified_equation(rk2)
+result = series.sum()
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
+
+start_time = time.time()
+BSeries.set_order(up_to_order)
+Y1 = BSeries.y() + midpoint_method.A[1,0] * BSeries.hf()
+rk2 = BSeries.y() + midpoint_method.b[0] * BSeries.hf() + midpoint_method.b[1] * BSeries.compo_hf(Y1)
+series = BSeries.modified_equation(rk2)
+result = series.sum()
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
+
+
+with open(io_file, 'a') as io:
+  print("\nModifying integrator", file=io)
+
+start_time = time.time()
+BSeries.set_order(up_to_order)
+Y1 = BSeries.y() + midpoint_method.A[1,0] * BSeries.hf()
+rk2 = BSeries.y() + midpoint_method.b[0] * BSeries.hf() + midpoint_method.b[1] * BSeries.compo_hf(Y1)
+series = BSeries.modifying_integrator(rk2)
+result = series.sum()
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
+
+start_time = time.time()
+BSeries.set_order(up_to_order)
+Y1 = BSeries.y() + midpoint_method.A[1,0] * BSeries.hf()
+rk2 = BSeries.y() + midpoint_method.b[0] * BSeries.hf() + midpoint_method.b[1] * BSeries.compo_hf(Y1)
+series = BSeries.modifying_integrator(rk2)
+result = series.sum()
+end_time = time.time()
+with open(io_file, 'a') as io:
+  print(result, file=io)
+  print("", end_time - start_time, "seconds", file=io)
